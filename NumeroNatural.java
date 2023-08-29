555415c60156a128c9ecd1e39652eaf2578713ed
Sergio Cerqueira <126781655+sergiocerq@users.noreply.github.com>
2023-08-28 22:45:00 -0300

    Add files via upload
    Numero natural em java

---
+++ NumeroNatural.java
@@ -0,0 +1,20 @@
+package questao2;
+import java.util.Scanner;
+
+public class NumeroNatural {
+
+	public static void main(String[] args) {=
+		Scanner teste = new Scanner(System.in);
+		System.out.println("Informe um valor: ");
+		int valor = teste.nextInt();
+		if(valor > 0) {
+			System.out.println("O número é positivo!");
+		} else if(valor == 0) {
+			System.out.println("O número é 0");
+		} else {
+			System.out.println("O número é negativo!");
+		}
+	
+	}
+
+}

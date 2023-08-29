86a5437f6d3ad727cbd9166d6651faaa0ba48181
Sergio Cerqueira <126781655+sergiocerq@users.noreply.github.com>
2023-08-28 22:45:27 -0300

    Update NumeroNatural

--- NumeroNatural.java
+++ NumeroNatural.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class NumeroNatural {
 
-	public static void main(String[] args) {=
+	public static void main(String[] args) {
 		Scanner teste = new Scanner(System.in);
 		System.out.println("Informe um valor: ");
 		int valor = teste.nextInt();

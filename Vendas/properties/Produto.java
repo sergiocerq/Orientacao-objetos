ef939761b2df542c678e608526fd606ff576a393
Sérgio Cerqueira <126781655+sergiocerq@users.noreply.github.com>
2023-11-08 01:49:49 -0300

    Update Produto.java

--- Vendas/properties/Produto.java
+++ Vendas/properties/Produto.java
@@ -1,5 +1,42 @@
 package properties;
 
 public class Produto {
+	
+	private int id;
+	private String nome;
+	private double preco;
+	
+	public Produto(int id, String nome, double preco) {
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String toString() {
+		return "ID: " + id + ", nome: " + nome + ", preco: " + preco;
+	}
 }

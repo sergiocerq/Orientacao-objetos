429a7b042fd93f2bc2d915dd82399b54cae2e098
Sérgio Cerqueira <126781655+sergiocerq@users.noreply.github.com>
2023-11-08 01:50:18 -0300

    Update DAOConexao.java

--- Vendas/JDBC/DAOConexao.java
+++ Vendas/JDBC/DAOConexao.java
@@ -1,5 +1,12 @@
 package JDBC;
 
-public interface DAOConexao {
+import java.sql.*;
+import java.util.List;
 
+public interface DAOConexao<T> {
+	T getValor() throws SQLException;
+	List<T> getTodosValores() throws SQLException;
+	boolean cadastraValor(T valor) throws SQLException;
+	boolean removeValor(T valor) throws SQLException;
+	boolean atualizaValor(T valor) throws SQLException;
 }

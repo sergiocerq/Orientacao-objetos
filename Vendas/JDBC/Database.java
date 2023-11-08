3446525f9a9f9214cb61afc91e5652983b4014a9
Sérgio Cerqueira <126781655+sergiocerq@users.noreply.github.com>
2023-11-08 01:50:56 -0300

    Update Database.java

--- Vendas/JDBC/Database.java
+++ Vendas/JDBC/Database.java
@@ -1,5 +1,19 @@
 package JDBC;
+import java.sql.*;
 
 public class Database {
+	
+	private static String url = "jdbc:mysql://localhost:3275:TESTE";
+	private static String user = "root";
+	private static String password = "123456";
+	
+	public Database() {
+	}
+	
+	public static Connection getConnection() throws SQLException {
+		Connection conn = DriverManager.getConnection(url, user, password);
+		return conn;
+	}
+	
 
 }

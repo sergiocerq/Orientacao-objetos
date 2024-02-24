af149bd1cea35619ace7d2dfa5d9792a27df7ee3
Sergio Cerqueira <126781655+sergiocerq@users.noreply.github.com>
2024-02-24 12:26:41 -0300

    feat: adicionado projeto do banco

---
+++ Banco/DAO/DAOCliente.java
@@ -0,0 +1,21 @@
+package DAO;
+
+import java.sql.*;
+import java.util.List;
+
+import entities.Cliente;
+
+public class DAOCliente implements DAOInterface {
+	private Connection conn;
+
+	public List getAll() throws SQLException {
+		List<Cliente> lista = null;
+	}
+
+	public boolean inserir(Object valor) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+}
